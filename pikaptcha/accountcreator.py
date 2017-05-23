efb2b712fdb03a658e06e888b1696d313b0c92d9
walaoaaa1234 <walaoaaa1234@hotmail.com>
2017-05-23 15:47:54 +0800

    hmmmmm

--- pikaptcha/accountcreator.py
+++ pikaptcha/accountcreator.py
@@ -108,7 +108,8 @@ def _validate_username(username):
 def run_chromedriver():
     try:
         if 'chromedriver' not in globals():
-            global chromedriver = Chrome()
+            global chromedriver
+            chromedriver = Chrome()
             chromedriver.set_window_size(600, 700)
         return
     except:
@@ -168,7 +169,7 @@ def create_account(username, password, email, birthday, captchakey2, captchatime
     # -----------------------------------------------------------
     dcap = dict(DesiredCapabilities.PHANTOMJS)
     dcap["phantomjs.page.settings.userAgent"] = user_agent
-    dcap["phantomjs.page.settings.loadImages"] = "false"
+    dcap["phantomjs.page.settings.loadImages"] = False
     #driver = webdriver.PhantomJS(desired_capabilities=dcap)
     driver = PhantomJS(desired_capabilities=dcap)
     # -----------------------------------------------------------
